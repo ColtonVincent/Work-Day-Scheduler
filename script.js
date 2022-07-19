82413e1da8ea2d75c73796c621a2aae06f4afc7a
alan <cvinny33@gmail.com>
2022-07-18 20:59:48 -0600

    added styling for text box

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("dddd MMM Do, YYYY"));
+
+var currentHour = moment().format("HH")
+
+$(".description").each(function(){
+var id = $(this).attr("id").split("-")[1]
+if(currentHour>id){
+    $(this).addClass("past")
+}
+else if(currentHour==id){
+    $(this).addClass("present")
+}
+else{
+    $(this).addClass("future")
+}
+ addEventListener
+
+localStorage.getItem("btn")
+})
